f4a55342cd753c67c250351ffb041bf7bcbf53bf
veroca88 <veronicadonoso1506@gmail.com>
2020-12-18 15:52:24 -0600

    Delete searchData function and Refactor handleclick function to display info below location input

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -3,39 +3,44 @@ import { useForm } from 'react-hook-form'
 import LOCATION_SERVICE from './services/LocationService'
 
 
-export default function SearchLocation(props) {
+export default function SearchLocation() {
   const { handleSubmit, errors, register } = useForm({
     defaultValues: {
       location: ""
     }
   })
   const [response, setResponse] = useState({ data: [] });
+  const [loadMessage, setLoadMessage] = useState(false)
+  const [weatherInfo, setWeatherInfo] = useState('Not info yet')
 
-    const searchData = (data, e) => {
-        LOCATION_SERVICE.search(data)
-        e.target.reset(e)
-    }
     
-    const handleClick = async () => {
-      const message = await LOCATION_SERVICE.search(response)
-      console.log("message handle click", message)
-
-    }
+    const handleClick = async() => {
+      const responseAPI = await LOCATION_SERVICE.search(response)
+      setLoadMessage(true)
+      setWeatherInfo(responseAPI.data)
+      console.log(weatherInfo)
+      }
 
     return (
-        <form className="location_form" onSubmit={handleSubmit(searchData)}>
+        <form onSubmit={handleSubmit(handleClick)}>
           <label className="location" htmlFor="location">
             Location: 
           </label>
           <input
-            aria-invalid={errors.name ? "true" : "false"}
-            name="location"
-            type="text"
-            placeholder="location"
-            ref={register({ required: true })}
-            ></input>
+          className="input_location"
+          aria-invalid={errors.name ? "true" : "false"}
+          name="location"
+          type="text"
+          placeholder=" Location"
+          ref={register({ required: true })}
+          required
+          ></input>
+            <div>
           {errors.location && <span>This field is required</span>}
-          <button type='submit' onClick={handleClick}>Search</button>
+            </div>
+            <div className="output_info">
+            {loadMessage? weatherInfo : null}
+            </div>
           </form>
     )
 }
